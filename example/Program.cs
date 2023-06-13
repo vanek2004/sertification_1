b54df830dff13c7dd8d2934ed3d3f91d2df37cdf
unknown <Ivan>
2023-06-13 21:06:58 +0700

    create the main foundation

--- example/Program.cs
+++ example/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.Write("Введите размер массива --> ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = CreateArray(size);
+PrintArray(array);
+
+string[] CreateArray(int length)
+{
+    string[] arr = new string[length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"'{arr[i]}'");
+    }
+    Console.Write("]");
+}
